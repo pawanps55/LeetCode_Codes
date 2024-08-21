35711a2ca5f15255b580c40223883457f2577b4b
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2024-08-21 15:13:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (7.14%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        /* int i;
+        long long int ans;
+        while(i<=n){
+        for(int i=1;i<n;i++){
+            ans = x*i;
+        }
+      } return ans; */ return pow(x,n);
+    }
+};
