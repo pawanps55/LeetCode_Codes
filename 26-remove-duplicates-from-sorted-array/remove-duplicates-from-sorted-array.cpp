9cdb1a93281b7fdba14e934d5723e116259d55a9
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2025-01-03 14:51:25 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (51.01%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums){  
-      //  vector<int>temp;
-        int j = 0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] != nums[j]){
-                 j++;
-                nums[j] = nums[i];
-               
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+            for(int j =1;j<nums.size();j++){
+                if(nums[i]!=nums[j]){
+                    nums[i+1]=nums[j];
+                    i++;
+                }
             }
-            
-        }
-       return j+1;
+        return i+1;
     }
 };
