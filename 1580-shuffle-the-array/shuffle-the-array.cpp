29da9baaf5eeaecbbbd58a2a909af2d1ce82ab7c
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2025-01-19 18:45:57 +0530

    Time: 4 ms (66.26%) | Memory: 13.4 MB (48.14%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> shuffle(vector<int>& nums, int n) {
      //   int  n = nums.size();
         vector<int>s;
-        for(int i=0;i<n;++i){
+        for(int i=0;i<n;i++){
            s.push_back(nums[i]);
            s.push_back(nums[i+n]);
         }
