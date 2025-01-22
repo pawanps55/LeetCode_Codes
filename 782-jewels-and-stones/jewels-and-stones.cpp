8774a94b66e9a2bc6d708be60448c70aa7ecb8ea
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2025-01-22 15:20:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (77.42%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int n = jewels.length();
+        int m = stones.length();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            char jewel = jewels[i];
+            for (int j = 0; j < m; j++) {
+                char stone = stones[j];
+                if (jewel == stone) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
