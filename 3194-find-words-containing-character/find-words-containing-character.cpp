e0079bf03acd3daa758ee363a54df354b9258ccb
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2025-01-22 15:44:08 +0530

    Time: 8 ms (5.92%) | Memory: 33.1 MB (63.25%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        int n = words.size();
+        vector<int>s;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<words[i].size();j++){
+                    if(words[i][j]==x){
+                        s.push_back(i);
+                        break;
+                    }
+            }
+        }
+        return s;
+    }
+};
