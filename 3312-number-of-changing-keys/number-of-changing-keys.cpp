d329a306e00918fc8a40f5c29d76c0741098d9b4
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2025-02-05 12:19:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (11.04%) - LeetSync

---
+++ 3312-number-of-changing-keys/number-of-changing-keys.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countKeyChanges(string s) {
+        int n = s.size();
+        int count = 0;
+        for(int i=0;i<n-1;i++){
+           if(tolower(s[i]) != tolower(s[i+1])){
+            count++;
+           } 
+        }
+        return count;
+    }
+};
