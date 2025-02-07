b52e28e45f1ca11c01db2f5ec24a7b47094dfe55
Pawan Sorte <156445826+pawanps55@users.noreply.github.com>
2025-02-07 14:38:22 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (15.38%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+
+        for(int i=0;i<details.size();i++){
+            if(details[i][11]=='6' && details[i][12]=='0')
+                continue;
+
+            else if(details[i][11]>='6'){
+                count++;
+            }
+            else{
+                count=count+0;
+            }
+        }
+        return count;
+    }
+};
